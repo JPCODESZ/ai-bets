2755257a1dd6ee225a0f589682adec38c6ef8fee
JPCODESZ <jonpeppy2000@gmail.com>
2025-03-25 19:17:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,9 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
+print("API started...")
+
+git add app.py
+git commit -m "Trigger redeploy"
+git push
