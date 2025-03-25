69e79984548f8cc12a883b42003b414d1b199e69
JPCODESZ <jonpeppy2000@gmail.com>
2025-03-25 19:06:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,8 @@ def bet():
     result = place_bet(data)
     return jsonify(result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
